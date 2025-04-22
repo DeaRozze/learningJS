2e90f43936bb72882dc265eb338d8997cce6a810
DeaRozze <vl4dkudi@gmail.com>
2025-04-22 12:54:53 +0300

    make placeholder

--- promisePractPart3.js
+++ promisePractPart3.js
@@ -449,3 +449,25 @@ Promise.resolve().then(() => console.log("D"));
 console.log("E");
 
 // A B E D C
+
+
+// next = previous * 16807 % 2147483647
+// Если мы используем 1 как зерно, то значения будут:
+
+// 16807
+// 282475249
+// 1622650073
+// …и так далее…
+// Задачей является создать функцию-генератор pseudoRandom(seed), которая получает seed и создаёт генератор с указанной формулой.
+
+// Пример использования:
+function* pseudoRandom(seed) {
+  yield (seed * 16807) % 2147483647;
+  yield (seed * 16807 * 16807) % 2147483647;
+  yield (seed * 16807 * 16807) * 16807 % 2147483647;                                             
+}
+let generator = pseudoRandom(1);
+
+alert(generator.next().value); // 16807
+alert(generator.next().value); // 282475249
+alert(generator.next().value); // 1622650073
